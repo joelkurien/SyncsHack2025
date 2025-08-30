cc7a920836324503fac77bfe12059259ed237e6e
Joelkurien <joelkurien7@gmail.com>
2025-08-31 01:04:07 +1000

    Added user login and registration logic

--- NeoEco/ChallengeSetter/urls.py
+++ NeoEco/ChallengeSetter/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("quests/transport", views.decideTransportOperation, name="decideTransportOperation"),
-    path("quests/volunteer", views.search_opportunities, name="search_opportunities")
+    path("quests/volunteer", views.search_opportunities, name="search_opportunities"),
+    path("user/register", views.register_user, name="register_user"),
+    path("user/login", views.user_login, name="user_login")
 ]
--- NeoEco/ChallengeSetter/views.py
+++ NeoEco/ChallengeSetter/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 import requests
 import pandas as pd
 import math
@@ -9,6 +13,7 @@ import asyncio, httpx
 import os
 from dotenv import load_dotenv
 import json
+from .models import User
 
 load_dotenv("./content.env")
 
@@ -19,6 +24,60 @@ TRAFFIC_API = os.getenv("TRAFFIC_KEY")
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+@api_view(['POST'])
+def register_user(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    email = request.data.get("email")
+    address = request.data.get("address1")
+    work_address = request.data.get("address2")
+
+    if not username or not password:
+        return Response({"error": "Username and password required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create(
+        username=username,
+        email=email,
+        password=password,  # important: never store raw passwords
+        address = address,
+        work_address = address
+    )
+
+    return Response({"message": "User created successfully", "user_id": user.id}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def user_login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({"error": "Wrong Username"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    if user.check_password(password):
+        return Response({"error": "Wrong Password"}, status=status.HTTP_401_UNAUTHORIZED)
+    token, created = Token.objects.get_or_create(user=user)
+
+    return Response({
+        "message": "Login successful",
+        "token": token.key,
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "eco_rank": user.eco_rank,
+            "xp": user.xp,
+            "level": user.level,
+            "avatar_choice": user.avatar_choice,
+        }
+    }, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def decideTransportOperation(request):
