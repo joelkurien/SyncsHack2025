d3b1b2bd7d691716477c632ff039d510d07b5f75
Joel John Kurien <55293875+joelkurien@users.noreply.github.com>
2025-08-30 16:02:17 +1000

    Added more scaffolding and location finder

--- NeoEco/ChallengeSetter/views.py
+++ NeoEco/ChallengeSetter/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello World")
+
+def decideTransportOperation(request):
+    pass
+
+def getLatLong(location):
+    try:
+        url = "https://photon.komoot.io/api/"
+        params={"q": location, "limit": 1}
+        latlongResp = requests.get(url, params=params)
+        data = latlongResp.json()
+        
+        if data and data['features']:
+            points = data['features'][0]['geometry'].get("coordinates")
+            long, lat = points[0], points[1]
+            return [long, lat]
+        else:
+            raise ValueError("Location is inValid")
+    except Exception as e:
+        return None
+
